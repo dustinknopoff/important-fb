2cb8bfd92d7e127cd33e29e72400717650307a40
Dustin Knopoff <dustinknopoff@gmail.com>
2019-01-05 11:36:27 -0500

    fix(manifest): name matches...not gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Important-fb`,
+        short_name: `fb-filter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
